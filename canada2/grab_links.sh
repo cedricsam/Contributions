f0e2bf18dcdfe43b54ca59600233a473c2121a1f
Cedric Sam <cedricsam@gmail.com>
2015-02-23 09:27:44 +0800

    fixes on grabbing links

--- canada2/grab_links.sh
+++ canada2/grab_links.sh
@@ -6,5 +6,5 @@ touch "links.${D}.txt"
 
 for i in `ls -rt ../*.html`
 do
-    grep -Eo '/WPAPPS/WPF/EN/PP/ContributionReport[^"]+' "${i}" | grep addrclientid | cut -d: -f2 | cut -d? -f2 >> links.${D}.txt
+    grep -Eo '/WPAPPS/WPF/EN/[A-Z]{2,}/ContributionReport[^"]+' "${i}" | grep addrclientid | cut -d: -f2 | cut -d? -f2 | sed 's/queryid=[^&]\+/queryid=/' | sed 's/&amp;/\&/g' >> links.${D}.txt
 done
